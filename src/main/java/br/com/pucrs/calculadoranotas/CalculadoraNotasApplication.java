6d0fa80abb17b7f88bd468873782bf6f8bf30d4e
Luca Gemelli <luccagemelli@hotmail.com>
2020-09-28 23:02:37 -0300

    - commit inicial

---
+++ src/main/java/br/com/pucrs/calculadoranotas/CalculadoraNotasApplication.java
@@ -0,0 +1,13 @@
+package br.com.pucrs.calculadoranotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraNotasApplication.class, args);
+	}
+
+}
